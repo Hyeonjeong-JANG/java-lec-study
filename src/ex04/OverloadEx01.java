9025f1583f93a60bd999e6ac538a85d7ba4dc91b
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2023-12-20 17:35:57 +0900

    객체 지향 문법

--- src/ex04/OverloadEx01.java
+++ src/ex04/OverloadEx01.java
@@ -7,5 +7,8 @@ public class OverloadEx01 {
         System.out.println(1.0);
         System.out.println("1");
         System.out.println();
+        // 위의 println() 메소드들은 이름이 모두 같다.
+        // 하지만 매개변수의 형태가 모두 다르기 때문에 메소드 이름이 같아도 상관 없다.
+        // 이것을 메소드 오버로딩이라고 한다.
     }
 }
