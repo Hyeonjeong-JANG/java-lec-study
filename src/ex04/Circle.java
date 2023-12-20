85f8861176d1ff111dd94f632c0c17b9a5e3e7aa
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2023-12-20 19:55:32 +0900

    Circle 수정

--- src/ex04/Circle.java
+++ src/ex04/Circle.java
@@ -3,13 +3,24 @@ package ex04;
 // 설계도
 public class Circle {
     private int radius;
+    private String color;
 
     // 생성자: 생성될 때 무조건 실행되는 메서드, 사람의 몸무게, 키 등 절대 없어서는 안 되는 것
     public Circle(int r) { // 얘는 안 만들면 눈에 안 보여도 자동으로 생성되어 있음
         radius = r;
     }
 
+    public Circle(String c) { // 얘는 안 만들면 눈에 안 보여도 자동으로 생성되어 있음
+        color = c;
+    }
+
     public double getArea() {
         return 3.4 * radius * radius;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+
 }
--- src/ex04/CircleTest.java
+++ src/ex04/CircleTest.java
@@ -4,7 +4,8 @@ public class CircleTest {
     public static void main(String[] args) {
         Circle obj = new Circle(100); // 이렇게 다른 파일에 있는 것도 불러진다.
         // Circle()은 Circle 파일에서는 static이 없기 때문에 떠있지 않지만
-
+        Circle obj2 = new Circle("red");
         System.out.println(obj.getArea());
+        System.out.println(obj2.getColor());
     }
 }
