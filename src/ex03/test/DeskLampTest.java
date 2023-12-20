cd50cb0ca430616793a4a65b991c41b6071b15aa
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2023-12-20 11:09:13 +0900

    객체지향 기본 문법

--- src/ex03/test/DeskLampTest.java
+++ src/ex03/test/DeskLampTest.java
@@ -6,9 +6,11 @@ public class DeskLampTest {
         DeskLamp myLamp = new DeskLamp();
 
         // 객체의 메소드를 호출하려면 도트 연산자인 .을 사용한다.
-        myLamp.turnOn();
         System.out.println(myLamp);
-        myLamp.turnOff();
-        System.out.println(myLamp);
+
+//        myLamp.turnOn();
+//        System.out.println(myLamp);
+//        myLamp.turnOff();
+//        System.out.println(myLamp);
     }
 }
