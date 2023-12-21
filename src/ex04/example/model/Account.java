0d97d6b50f54fd2abf7dfda50e5bf6f08b3c1aad
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2023-12-21 11:47:34 +0900

    계쫘이체 테스트 완료

--- src/ex04/example/model/Account.java
+++ src/ex04/example/model/Account.java
@@ -1,10 +1,10 @@
 package ex04.example.model;
 
 public class Account {
-    final int id; // 계좌 번호(제약 조건: 숫자 4자리로 만들어져 있다.) 지금은 4자리니까 int로 했다. 계좌 번호는 번호지만 그 길이가 길기 때문에 String으로 만든다. 연산할 일이 없으니까.
+    public final int id; // 계좌 번호(제약 조건: 숫자 4자리로 만들어져 있다.) 지금은 4자리니까 int로 했다. 계좌 번호는 번호지만 그 길이가 길기 때문에 String으로 만든다. 연산할 일이 없으니까.
     // final을 하려면 반드시 그 값이 초기화 되어야 한다. 그렇지 않으면 오류가 난다.
-    long balance; //  int로 만들면 21억 얼마만 들어가기 때문에 long으로 해야 한다.
-    int userId;
+    public long balance; //  int로 만들면 21억 얼마만 들어가기 때문에 long으로 해야 한다.
+    public int userId;
 
     public Account(int id, long balance, int userId) {
         this.id = id;
