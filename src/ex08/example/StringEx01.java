5a36468aad4605f342ba233ce18937b19e8fde33
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-01-02 09:27:12 +0900

    코드 완성

--- src/ex08/example/StringEx01.java
+++ src/ex08/example/StringEx01.java
@@ -1,42 +1,116 @@
 package ex08.example;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class StringEx01 {
+    static String data = "";
+    static String encodingData = "";
+
     public static String encoding(String data) {
-        return "";
-    }
+        int count;
+        char head = 0;
 
-    public static String decoding(String encodingData) {
-        return "";
-    }
+        int dataLength = data.length();
 
-    // 데이터를 인코딩, 디코딩 하는 작업
-    // 주어진 문자열의 첫 번째 글자가 몇 개이고 그게 몇 번째까지 같은지 확인.
-    // 달라지면 그 달라지는 글자가 뭔지 확인
-    
-    public static void main(String[] args) {
-        String data = "AABBBCCCCDDDDD";
-        int dataSize = data.length();
-//        System.out.println(dataSize);
-        char orderChar = data.charAt(0);
-//        System.out.println(orderChar);
+        char[] dataArr = new char[dataLength];
+        for (int i = 0; i < dataLength; i++) {
+            dataArr[i] = data.charAt(i);
+        }
 
-        int count = 0;
-        char char01 = 0, char02 = 0;
+        char branchPoint = dataArr[0]; // 데이터 배열의 첫 번째 것을 일단 분기점으로 잡아
 
-        for (int i = 0; i < dataSize; i++) {
-            if (data.charAt(0) == data.charAt(i)) {
-                char01 = data.charAt(0);
-                count++;
+        ArrayList<Character> branchPointList = new ArrayList<>(); // 분기점들을 담은 리스트
+        branchPointList.add(branchPoint); // 분기점 리스트에 데이터 배열의 첫 번째 것을 넣음
+
+        // 각 글자들이 처음 나타나는 지점 찾기
+        ArrayList<Integer> branchOrder = new ArrayList<>(); // 각 분기점들의 인덱스를 담은 리스트
+        for (int i = 0; i < dataLength - 1; i++) { // 0부터 시작해서 그 다음 녀석과 비교해야 하니까 1을 뺌
+            if (dataArr[i] != dataArr[i + 1]) {
+                branchPoint = dataArr[i + 1];
+                branchPointList.add(branchPoint);
             }
-            if (data.charAt(0) != data.charAt(i)) {
-                System.out.println(data.charAt(i));
+
+            // 인덱스를 찾아서 어레이리스트에 넣음
+            int index = data.indexOf(branchPoint); // 분기점들이 몇 번째 순서에 있는지 인덱스를 찾아서
+            branchOrder.add(index); // 넣음
+        }
+
+        //System.out.println("분기점: " + branchPointList);
+
+        // 해시셋으로 중복되는 숫자를 제거
+        HashSet<Integer> uniqueSet = new HashSet<>(branchOrder);
+        ArrayList<Integer> uniqueBranchOrder = new ArrayList<>(uniqueSet);
+
+        // 위에 찾은 문자와 숫자를 토대로 아래의 코드를 다시 만들기!!!
+        ArrayList<String> encodingList = new ArrayList<>();
+        String headStr = " ";
+        String countStr = " ";
+
+/////////////////////배열 전체 돌기
+        for (int j = 0; j < branchPointList.size(); j++) {
+            count = 1;
+            for (int i = uniqueBranchOrder.get(j); i < dataLength; i++) {
+                if ((branchPointList.get(j) == dataArr[i]) && (i != uniqueBranchOrder.get(j))) {
+                    head = branchPointList.get(j);
+                    headStr = Character.toString(head);
+                    count++;
+                    countStr = Integer.toString(count);
+
+                }
+            }
+            encodingList.add(headStr);
+            encodingList.add(countStr);
+        }
+        String encodingData = String.join("", encodingList); // 인코딩 된 데이터라는 뜻.
+        return encodingData;
+    }
+
+    public static StringBuilder decoding(String encodingData) {
+        int codeLength = encodingData.length();
+        char[] codeArr = new char[codeLength];
+
+        for (int i = 0; i < codeLength; i++) {
+            codeArr[i] = encodingData.charAt(i);
+        }
+
+        // 짝수에 들어가는 애는 분기점, 홀수에 들어가는 애는 몇 번 반복
+        char branchChar = 0;
+        char countChar = 0;
+        ArrayList<Character> branchCharList = new ArrayList<>();
+        ArrayList<Integer> countIntList = new ArrayList<>();
+        ArrayList<Character> decodingList = new ArrayList<>();
+
+        int countInt = 0;
+
+        for (int i = 0; i < codeArr.length; i++) {
+            if (i % 2 == 0) {
+                branchCharList.add(codeArr[i]);
+            }
+            if (i % 2 != 0) {
+                countIntList.add(Character.getNumericValue(codeArr[i]));
             }
         }
 
-        System.out.print("문자 " + char01 + "는 ");
-        System.out.println(count + "개");
+        for (int j = 0; j < countIntList.size(); j++) {
+            for (int i = 0; i < countIntList.get(j); i++) {
+                decodingList.add(branchCharList.get(j));
+            }
+        }
 
-        String convert = "A2B3C4D5"; // Byte -> 8Byte
+        // 어레이리스트 문자열로 변환하기
+        StringBuilder decoding = new StringBuilder();
+        for (char c : decodingList) {
+            decoding.append(c);
+        }
+        return decoding;
+    }
+
+    public static void main(String[] args) {
+        data = "AABBBCCCCDDDDDDRRRㅁㅁㅁaaa777";
+        System.out.println(encoding(data));
+        encodingData = encoding(data);
+        System.out.println(decoding(encodingData));
 
     }
 }
